878e683daf6a6cb08849dafdd2e2765441574958
hirasarfaraz <hirasarfaraz712@gmail.com>
2026-02-11 06:18:01 -0800

    Add files via upload

---
+++ assigment.py
@@ -0,0 +1,65 @@
+# Part A:
+#1
+name = "Hira sarfaraz khan"
+print(name)
+
+#2
+age = 18
+height = 4.6 
+print(age,height)
+
+#3
+a = 5
+print(type(a))
+
+#4
+is_student = True
+print(is_student)
+
+#5
+user_num = input("Enter a number: ")
+print(user_num)
+
+# Part B:
+#1
+num1 = int(input("Enter a num1:"))
+num2 = int(input("Enter num2:"))
+print(num1+num2)
+
+#2
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+if a>b:
+    print(a)
+else :
+    print(b)
+
+#3
+a = int(input("Enter a :"))
+if a % 2 == 0 :
+    print("Even num")
+else:
+    print("Odd num")
+
+#4
+a = int(input("Enter a num : "))
+if a >= 50:
+    print("Pass")
+else:
+    print("Fail")
+
+#5
+a = int(input("Enter a: "))
+if a < 10:
+    print("Less than 10 ")
+elif a == 10:
+    print("Equal to 10")
+else :
+    print("Greather than 10")
+
+#Part C:
+num = int(input("Enter a number : "))
+if num >= 0 :
+    print("Positivr number")
+else:
+    print("Negative number")
